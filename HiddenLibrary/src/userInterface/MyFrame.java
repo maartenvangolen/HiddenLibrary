0b24788472455132c8f02e6fc0d80a905144c4f1
maartenvangolen <zarrokvangolen@gmail.com>
2015-11-30 18:16:18 +0100

    All UI components for the main functionality is created.

--- HiddenLibrary/src/userInterface/MyFrame.java
+++ HiddenLibrary/src/userInterface/MyFrame.java
@@ -1,31 +1,56 @@
 package userInterface;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class MyFrame extends JFrame implements ActionListener {
 	private JButton encrypt, decrypt;
+	private JButton commandlineOn, commandlineOff;
 	private JTextField input, display;
+	private JLabel commandLable;
 
 	public MyFrame() {
 		setLayout(new FlowLayout());
 
 		input = new JTextField();
 		display = new JTextField();
+		display.setEditable(false);
+
+		input.setPreferredSize(new Dimension(200, 20));
+		display.setPreferredSize(new Dimension(200, 20));
 
 		encrypt = new JButton("Encrypt");
 		decrypt = new JButton("Decrypt");
+		commandlineOn = new JButton("ON");
+		commandlineOff = new JButton("OFF");
+		commandlineOff.setEnabled(false);
+		
+		encrypt.setPreferredSize(new Dimension(100,25));
+		decrypt.setPreferredSize(new Dimension(100,25));
 
 		decrypt.addActionListener(this);
 		encrypt.addActionListener(this);
+		commandlineOn.addActionListener(this);
+		commandlineOff.addActionListener(this);
+
+		commandLable = new JLabel("Commandline:");
+
+		add(input);
+		add(display);
 
 		add(encrypt);
 		add(decrypt);
+
+		add(commandLable);
+		add(commandlineOn);
+		add(commandlineOff);
 
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -37,5 +62,9 @@ public class MyFrame extends JFrame implements ActionListener {
 		}
 		if (e.getSource() == decrypt) {
 		}
+	}
+
+	public static void main(String[] args) {
+		MyFrame mf = new MyFrame();
 	}
 }
