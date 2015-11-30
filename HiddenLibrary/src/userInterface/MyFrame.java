3b13c4a150053773fd522075e70bf41fca2404fd
maartenvangolen <zarrokvangolen@gmail.com>
2015-11-30 18:00:15 +0100

    Filled the MyFrame with some components

--- HiddenLibrary/src/userInterface/MyFrame.java
+++ HiddenLibrary/src/userInterface/MyFrame.java
@@ -1,5 +1,41 @@
 package userInterface;
 
-public class MyFrame {
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class MyFrame extends JFrame implements ActionListener {
+	private JButton encrypt, decrypt;
+	private JTextField input, display;
+
+	public MyFrame() {
+		setLayout(new FlowLayout());
+
+		input = new JTextField();
+		display = new JTextField();
+
+		encrypt = new JButton("Encrypt");
+		decrypt = new JButton("Decrypt");
+
+		decrypt.addActionListener(this);
+		encrypt.addActionListener(this);
+
+		add(encrypt);
+		add(decrypt);
+
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(270, 200);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == encrypt) {
+		}
+		if (e.getSource() == decrypt) {
+		}
+	}
 }
