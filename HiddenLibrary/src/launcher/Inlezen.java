3e7aa993063bf166007a37b0927b79943986ff05
perdignus <gemineavanimeg@gmail.com>
2015-12-01 09:33:54 +0100

    Inlezen object gemaakt, foutmelding gekregen bij runnen

--- HiddenLibrary/src/launcher/Inlezen.java
+++ HiddenLibrary/src/launcher/Inlezen.java
@@ -1,25 +1,40 @@
 package launcher;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class Inlezen {
 	private ArrayList<Woord> woorden;
+
 	public Inlezen() {
 
 	}
-	
-	public void controleer(String kastLocatie){
-		String root ="C:/Users/floris/Desktop/crypt/project/";
-		File bieb = new File(root+"bieb");
-		File kast = new File(root+kastLocatie+"1");
-		
-//		ObjectInputStream ois = new ObjectInputStream(); 
-		if(!bieb.exists()){
-			if(kast.exists()){
+
+	public void controleer(String kastLocatie) throws IOException, ClassNotFoundException {
+		String root = "C:/Users/floris/Desktop/crypt/project/";
+		File bieb = new File(root + "bieb");
+		File kast = new File("C:/Users/floris/Desktop/crypt/project/"+kastLocatie+".txt");
+		FileInputStream fis = new FileInputStream(kast);
+		ObjectInputStream ois=new ObjectInputStream(fis);
 				
-			}
-		}
+				System.out.println((String)ois.readObject());
+				ois.close();
+				fis.close();
+			
+	}
+
+	public ArrayList<Woord> getWoorden() {
+		return woorden;
+	}
+
+	public void setWoorden(ArrayList<Woord> woorden) {
+		this.woorden = woorden;
+	}
+
+	public void addWoorden(Woord woord) {
+		woorden.add(woord);
 	}
 }
