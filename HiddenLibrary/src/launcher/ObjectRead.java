0587d50cbefc5c8bfa159f87825b2d1cae6249e0
perdignus <gemineavanimeg@gmail.com>
2015-12-02 19:56:55 +0100

    bezig

--- HiddenLibrary/src/launcher/ObjectRead.java
+++ HiddenLibrary/src/launcher/ObjectRead.java
@@ -1,7 +1,9 @@
 package launcher;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 
@@ -9,12 +11,12 @@ public class ObjectRead {
 	public Object readObject(File bestand) {
 		Object objecten = null;
 		try {
-			FileInputStream fis = new FileInputStream(bestand);
-			ObjectInputStream ois = new ObjectInputStream(fis);
-			objecten = (Object) ois.readObject();
+			FileReader fis = new FileReader(bestand);
+			BufferedReader ois = new BufferedReader(fis);
+			objecten = (Object) ois.readLine();
 			fis.close();
 			ois.close();
-		} catch (IOException | ClassNotFoundException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
