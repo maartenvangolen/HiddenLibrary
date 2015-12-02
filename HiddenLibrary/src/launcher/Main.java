e7573ab9d182dd5a41c5587f9d48b09ee24eabfa
perdignus <gemineavanimeg@gmail.com>
2015-12-02 19:57:06 +0100

    derp

--- HiddenLibrary/src/launcher/Main.java
+++ HiddenLibrary/src/launcher/Main.java
@@ -1,9 +1,19 @@
 package launcher;
 
+import java.io.IOException;
+
 public class Main {
 	public static void main(String[] args){
 		System.out.println("start");
 		Inlezen i = new Inlezen();
-		
+		try {
+			i.controleer("test");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
